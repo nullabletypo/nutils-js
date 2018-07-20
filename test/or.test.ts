b987e5ae9cc7c546eafdeda790b31c41e2e0fb95
nullabletypo <masahirokato.dev@gmail.com>
2018-07-20 20:41:15 +0900

    feat(or)

---
+++ test/or.test.ts
@@ -0,0 +1,12 @@
+import { or } from '../src/or'
+import { isString, isNull, isNumber } from '../src/lang'
+
+
+test('or', () => {
+  const pred = or(isString, isNumber, isNull)
+  expect(pred('')).toBe(true)
+  expect(pred(1)).toBe(true)
+  expect(pred(null)).toBe(true)
+  expect(pred(undefined)).toBe(false)
+  expect(pred({})).toBe(false)
+})
