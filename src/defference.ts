98ceee8957a425a762dfa5dc0772297621a759ed
nullabletypo <masahirokato.dev@gmail.com>
2018-07-21 16:40:20 +0900

    test(difference)

---
+++ src/defference.ts
@@ -0,0 +1,15 @@
+import { has } from './has'
+import { contains } from './contains'
+import { idx } from './idx'
+/**
+ * a[] - b[]
+ */
+type KM<T> = (val: T) => string
+
+export function difference<T>(a: T[], b: T[], getKey?: KM<T>) {
+  if (getKey == undefined) {
+    return a.filter(el => !contains(b, el))
+  }
+  const bHash = idx(b, { key: getKey })
+  return a.filter(el => !has(bHash, getKey(el)))
+}
---
+++ test/difference.test.ts
@@ -0,0 +1,16 @@
+import { difference } from '../src/defference'
+
+test('primitive', () => {
+  const a = [1, 2, 3, 4]
+  const b = [3, 4, 5, 6]
+  expect(difference(a, b)).toEqual([1, 2])
+})
+
+test('object', () => {
+  const a = [{ id: 1 }, { id: 2 }]
+  const b = [{ id: 1 }, { id: 3 }]
+  const r1 = difference(a, b, el => el.id + '')
+  const r2 = difference(a, b)
+  expect(r1).toEqual([{ id: 2 }])
+  expect(r2).toEqual([{ id: 1 }, { id: 2 }])
+})
