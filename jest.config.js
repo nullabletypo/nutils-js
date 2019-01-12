54ce41c925a101bcb1247d41e6d827239555784e
nullabletypo <masahirokato.dev@gmail.com>
2019-01-12 11:40:03 +0900

    chore: fix jest.config

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,4 @@
 module.exports = {
   notify: true,
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  },
-  testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
-  moduleFileExtensions: [
-    'ts',
-    'tsx',
-    'js',
-    'json',
-  ],
-  globals: {
-    'ts-jest': {
-      skipBabel: true,
-    },
-  },
+  preset: 'ts-jest'
 }
